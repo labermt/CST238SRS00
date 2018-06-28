78395f61f620d4c410016e111e52697fa0359428
cobegreeneoit <cobe.greene@oit.edu>
2018-06-27 20:54:01 -0700

    Completed The First App Tutorial.

---
+++ MyFirstApp/app/src/main/java/com/example/cobeg/myfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,25 @@
+package com.example.cobeg.myfirstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+
+    /** Code Taken From: https://developer.android.com/training/basics/firstapp/starting-activity#java */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        // Get the Intent that started this activity and exxtract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+    }
+}
