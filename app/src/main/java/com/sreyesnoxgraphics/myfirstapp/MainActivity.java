58ea0b33c948f62f86c8e6f804ef26a89c18a382
Silverio Reyes <32918848+SilverioReyes@users.noreply.github.com>
2018-06-29 02:48:45 -0700

    Functionality for displaying user input
    Create a seperate activity in which an intent is passed from the main activity once the user click on the "Send" button.
    
    A seperate screen will appear displaying the message that the user entered.

--- app/src/main/java/com/sreyesnoxgraphics/myfirstapp/MainActivity.java
+++ app/src/main/java/com/sreyesnoxgraphics/myfirstapp/MainActivity.java
@@ -1,13 +1,33 @@
 package com.sreyesnoxgraphics.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EXTRA_MESSAGE = "com.sreyesnoxgraphics.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**********************************************************************
+     * Purpose: This function gets the users input and sends it to display
+     *          on another screen (activity)
+     *
+     * Pre-condition: Text message not filled (Send button never handled).
+     *
+     * Post-condition: Displays msg to user (Send button handled).
+     ************************************************************************/
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText)findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
